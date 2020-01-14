68caabd0d4a6318fbad3284cd5e7f3536fba0136
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-01-15 01:00:15 +0530

    Added images and initial welcome page - comingsoon and raynTours

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './images/logo.svg';
+import carTransport from './images/carTransport.svg';
+import raynTours from './images/raynTours.png';
+import comingsoon from './images/comingsoon.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={carTransport} className="App-logo" alt="carTransport" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        For car rentals BookMyCars.in &nbsp;&nbsp; <img src={comingsoon} alt="comingsoon" /> 
         </p>
+        <p> For Inbound and Outbound tours and travels - click &nbsp;&nbsp;   
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://rayntourism.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        <img src={raynTours} alt="raynTours" />
         </a>
+        </p>
       </header>
     </div>
   );
