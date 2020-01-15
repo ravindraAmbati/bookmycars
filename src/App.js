ce69da8cab0d81329011941e76205d10b44a2506
ravindraAmbati <ravindra.reddy.ambati@outlook.in>
2020-01-15 22:50:38 +0530

    Created web app layout header, body, footer and content in body

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React from 'react';
-// import logo from './images/logo.svg';
-import carTransport from './images/carTransport.svg';
-import raynTours from './images/raynTours.png';
-import comingsoon from './images/comingsoon.jpg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Header from './components/header';
+import Body from './components/body';
+import Footer from './components/footer';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={carTransport} className="App-logo" alt="carTransport" />
-        <p>
-        For car rentals BookMyCars.in &nbsp;&nbsp; <img src={comingsoon} alt="comingsoon" /> 
-        </p>
-        <p> For Inbound and Outbound tours and travels&nbsp;   
-        <a
-          className="App-link"
-          href="https://rayntourism.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <img src={raynTours} alt="raynTours" />
-        </a>
-        </p>
-      </header>
-      <footer className="App-powered">powered by RRA</footer>
-    </div>
+      <Container className="App">
+        <Row>
+          <Col>
+            <Header/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            &nbsp;
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Body/>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Footer/>
+          </Col>
+        </Row>
+      </Container>
   );
 }
 
